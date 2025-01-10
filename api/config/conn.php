f44494f63505899fb045b40b7840b5a4eeab95a5
NookThana <nookkub70@gmail.com>
2025-01-10 16:45:53 +0700

    Update conn.php

--- api/config/conn.php
+++ api/config/conn.php
@@ -12,9 +12,8 @@ header("Access-Control-Allow-Credentials: true");
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=584244013_nook", $username, $password);
-  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- // echo "Connected successfully";
+  $conn->exec("SET NAMES 'utf8mb4'");
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
